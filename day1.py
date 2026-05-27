53d7821e60cdf1149a8cd3a1b4a4ad9d468a0b0d
Neeraj Katheriya <111559978+NeerajKatheriya1435@users.noreply.github.com>
2026-05-27 12:58:09 +0530

    first commit

---
+++ day1.py
@@ -0,0 +1,42 @@
+# print("Hello Word \nAniket \nhow are you")
+# print('Hello Word \\"neeraj how""" are you')
+
+# Here is simple print statent
+# print("hello")
+
+# Logic for prime number
+
+# himanshu=56
+# himanshukumar=56
+
+# HimanshuKumar=56 (Pascal Convention)
+# himanshuKumar=56 (Camel Case Convention)
+# addTwoNumber
+# print(HimanshuKumar)
+
+# dfghj54767=7
+# print(dfghj54767)
+# print("Helo word")
+
+rohan=6 
+rohan=45
+ROHAN=34
+# rohan!=ROHAN
+print(ROHAN)
+# var1=[56,89,67,56,56,45]
+
+# var1=5
+# var2=6.89
+
+# print(type(var1))
+# print(type(var2))
+# result=var1+var2
+
+# save1=int(result)
+# print(save1)
+
+# print(type(save1))
+var1 = 7j+6
+
+print(var1)
+print(type(var1))
