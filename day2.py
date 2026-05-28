857aac884586804107fc93638c70dca51f45d8fb
Neeraj Katheriya <111559978+NeerajKatheriya1435@users.noreply.github.com>
2026-05-28 13:55:10 +0530

    opeartors and strings methods

---
+++ day2.py
@@ -0,0 +1,147 @@
+
+# num1=7
+# num2=8
+# print(num1+num2)
+
+# name="Aniket"
+# friend1="Rohan"
+# friend2="Keshav"
+
+# print(name+"\n"+friend1)
+# print(name,friend1,friend2,sep="-")
+
+# print(name,end=" ")
+# print(friend1,end=" ")
+# print(friend2)
+
+# Taking Input from user
+
+# age=input("Enter you age: ")
+# print("My age is:",age)
+
+# num1=int(input("Enter num1: "))
+# num2=int(input("Enter num2: "))
+
+# num1=float(input("Enter num1: "))
+# num2=float(input("Enter num2: "))
+
+# print("The sum is",(num1+num2))
+
+# num1=int(input("Enter num1: "))
+# num2=int(input("Enter num2: "))
+
+# print(num1+num2)
+# print(num1-num2)
+# print(num1*num2)
+# print(num1/num2)
+
+# print(num1//num2)
+# print(num1%num2)
+# print(num1**num2)
+
+
+#Comparison Operators (Returns True or False)
+
+# print(5>6)
+# print(5>=5)
+# print(5<6)
+# print(5<=6)
+# print(5==5)
+# print(5!=6)
+
+#3. Assignment Operators
+
+# num1=7
+# # num1=num1+5
+# num1+=5
+# num1-=3
+# num1*=2
+# num1/=6
+# print(num1)
+
+# print(5<=8 and 23>90)
+# print(5<8 and 23>=23)
+# print(5>8 or 23>90)
+# print(not(5<8 or 23>90))
+
+# str1="Aniket " \
+# "Mishra"
+# str1="""
+# This my         string
+# how are    you
+# app aksie ho
+# """
+# print(str1)
+
+# print("My name is:",str1)
+
+# str1="Aniketttt"
+
+# for item in str1:
+#     print(item)
+
+# str1="Aniket Kumar Mishra"
+#     0123456789
+
+# print(str1[3:5])
+# print(str1[5:15])
+# print(str1[7:])
+# print(str1[:6])
+# print(str1[::2])
+# print(str1[-1])
+# print(str1[-5:-1])
+
+# str1="ANikeT"
+# print(str1.upper())
+# print(str1.lower())
+
+# str2 = "   Ayush Spoon   "
+# # print(str2)
+# print(str2.strip())
+
+# str3 = "!!!Hello !!!"
+# print(str3.strip("!"))
+
+# str2 = "Silver Spoon-Aniket Rohan"
+# print(str2.split("-"))
+# print(str2.replace("Sp", "M"))
+
+# str1 = "hello"
+# capStr1 = str1.capitalize()
+# print(capStr1)
+# str2 = "hello WorlD"
+# capStr2 = str2.capitalize()
+# print(capStr2)
+
+# str1 = "Welcome to the Console!!!"
+# print(str1.center(50))
+
+# str2 = "Anikett"
+# countStr = str2.count("t")
+# print(countStr)
+# print(len(str2))
+
+# str1 = "Welcome to the Console !!!"
+# print(str1.endswith("end"))
+
+# str1 = "He's name is Dan. He an honest man."
+# print(str1.find("is",11))
+
+# str1 = "He's name is Dan. Dan is an honest man."
+# print(str1.index("Dan45"))
+
+# str1 = "WelcomeToTheConsole"
+# print(str1.isalnum())
+# print(str1.isalpha())
+
+# str1 = "We wish you a \n Merry Christmas"
+# print(str1.isprintable())
+
+# str1 = "World Health Organization"
+# print(str1.istitle())
+
+# str1 = "Python is a Interpreted Language"
+# print(str1.swapcase())
+
+# str1 = "He is name is Dan. Dan is an honest man."
+# print(str1.title())
