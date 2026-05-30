cf2d381164260d4f64837527a173e6ce0d177583
Neeraj Katheriya <111559978+NeerajKatheriya1435@users.noreply.github.com>
2026-05-30 12:48:59 +0530

    break and continue and functions

---
+++ day4.py
@@ -0,0 +1,44 @@
+# for i in range(11):
+#     if(i==5):
+#         break
+#     print(i)
+
+# for i in range(11):
+#     if(i==5 or i==7):
+#         continue
+#     print(i)
+
+# i=5
+# while(i<11):
+#     print(i)
+#     i+=1
+
+# def addTwoNum(num1,num2):
+
+# def greet():
+#     print("Good Morning")
+
+# greet()
+
+
+# greet()
+
+# def addTwoNum(num1,num2): # Formal Parameters
+#     print("The sum is:",(num1+num2))
+
+# # def addTwoNum():
+# #     num1=8
+# #     num2=5
+# #     print("The sum is:",(num1+num2))
+
+# addTwoNum(5,6) # Actual Parameters
+# addTwoNum(2,8) 
+
+# def fact(num):
+#     ft=1
+#     for i in range(1,num+1):
+#         ft=ft*i
+#     return ft
+
+# print(fact(6))
+# print(fact(4))
