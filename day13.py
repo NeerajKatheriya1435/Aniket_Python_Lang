3799f462ade30bbd7e9d39d1a2bbf6105733b9c2
Neeraj Katheriya <111559978+NeerajKatheriya1435@users.noreply.github.com>
2026-06-09 14:06:34 +0530

    practice question and eqlity check

---
+++ day13.py
@@ -0,0 +1,17 @@
+
+
+# num1=78
+# num2=56
+# print(num1==num2)
+
+# num1=7
+# num2=7
+
+# l11=[4,6,8]
+# l22=[4,6,8]
+
+# print(num1==num2)
+# print(num1 is num2)
+
+# print(l11==l22)
+# print(l11 is l22)
---
+++ day14Practice.py
@@ -0,0 +1,51 @@
+
+l1=[4,7,2,7,9,6,78,56,5,98,34,90]
+
+# # print()
+# max=l1[0]
+# for item in l1:
+#     if(item<max):
+#         max=item  # max=7
+
+# print(max)
+
+# l1=[4,7,2,7,7,7,5,4,90]
+
+# s1=set(l1)
+# l1=list(s1)
+# print(l1)
+# unique=[]
+# for item in l1:
+#     if item not in unique:
+#         unique.append(item)
+
+# print(unique)
+# i=0
+# while (i<5):
+#     inp1=int(input("Enter the value: "))
+#     unique.append(inp1)
+#     i+=1
+
+# print(unique)
+
+# def fibb(num):
+#     if(num==0):
+#         return 0
+#     if(num==1):
+#         return 1
+    
+#     return fibb(num-1)+fibb(num-2)
+
+# term=7
+# # num=
+# print(fibb(term))
+
+# num=153
+# temp=num
+# count=0
+# while(num<0):
+#     num//=10
+
+
+
+
